e86ce13c00f911c9e3080773ffb20b7b8e031784
Iryna Pakrouskaya <irina267@mail.ru>
2017-04-16 23:44:59 +0300

    Task1-transposedMatrix

---
+++ transposedMatrix/Matrix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace transposedMatrix
+{
+    public static class Matrix
+    {
+        public static int[,] Fill(int length, int width)
+        {
+            int[,] filledMatrix = new int[length, width];
+            Random rand = new Random();
+            for (int i = 0; i < length; i++)
+                for (int j = 0; j < width; j++)
+                {
+                    filledMatrix[i, j] = rand.Next(20);
+                }
+            return filledMatrix;
+        }
+        public static void Print(int[,] matrix, int length, int width)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write("{0} ", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+        public static int[,] Transpose(int[,] matrix, int length, int width)
+        {
+            int[,] transposedMatrix = new int[width, length];
+            for (int i = 0; i < length; i++)
+                for (int j = 0; j < width; j++)
+                {
+                    transposedMatrix[j, i] = matrix[i, j];
+                }
+            return transposedMatrix;
+        }
+    }
+}
---
+++ transposedMatrix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace transposedMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 5;
+            int width = 6;
+
+            int[,] matrix = Matrix.Fill(length, width); 
+
+            Matrix.Print(matrix, length, width);
+            Console.WriteLine();
+
+            int[,] transposedMatrix = Matrix.Transpose(matrix, length, width);
+            Matrix.Print(transposedMatrix, width, length);            
+
+            Console.ReadLine();              
+        }
+    }
+}
